783d1411556d4b4868f30bac78a6df11299c01a4
dbrenes90 <daniel.brenes@my.tccd.edu>
2022-03-02 08:47:22 -0600

    added xml

--- StoreDL/IRepo.cs
+++ StoreDL/IRepo.cs
@@ -4,27 +4,77 @@ namespace StoreDL
 {
     public interface IRepo
     {
+        /// <summary>
+        /// Verifies Customer Pin matches Customer Id
+        /// </summary>
+        /// <param name="p_id"> Customer Id</param>
+        /// <param name="p_pin"> Customer Pin</param>
+        /// <returns> True if Valid </returns>
         Boolean VerifyCustomer(int p_id, string p_pin);
+        /// <summary>
+        /// Verifies Manager Pin matches Manager Id
+        /// </summary>
+        /// <param name="p_id"> Manager Id</param>
+        /// <param name="p_pin">Manager Pin</param>
+        /// <returns> True if Valid </returns>
         Boolean VerifyManager(int p_id, string p_pin);
-        List<Customer> SearchCustomer(string p_inputString);
-        List<Customer> SearchCustomerById(int p_id);
+        /// <summary>
+        /// Searches customer By email
+        /// </summary>
+        /// <param name="p_email"> customer email</param>
+        /// <returns> The list of customers</returns>
         List<Customer> SearchCustomerByEmail(string p_email);
-        // List<Product> GetProductById(int p_id);
-        // StoreFront AddStoreFront(StoreFront p_store);
-        // StoreFront GetStoreFront(int p_id);
+        /// <summary>
+        /// Places an order
+        /// </summary>
+        /// <param name="p_order"> The order to be placed </param>
+        /// <returns></returns>
         Order PlaceOrder(Order p_order);
-        // Order PlaceOrder(List<LineItem> p_lineitemlist, double p_total, int p_customerid, int p_storefrontid);
+        /// <summary>
+        /// Gets a list of orders
+        /// </summary>
+        /// <returns> The List of orders</returns>
         List<Order> GetAllOrders();
-        List<Order> GetOrderHistoryByStore(int p_id);
+        /// <summary>
+        /// Gets Line Items By Order Id
+        /// </summary>
+        /// <param name="p_orderId"> the OrderId</param>
+        /// <returns> The List of Line items </returns>
         List<LineItem> GetLineItemByOrderId(int p_orderId);
+        /// <summary>
+        /// Gets All StoreFronts
+        /// </summary>
+        /// <returns> List of Store Fronts </returns>
         List<StoreFront> GetAllStoreFront();
+        /// <summary>
+        /// Gets Inventory by Store
+        /// </summary>
+        /// <param name="p_id"> StoreFront id </param>
+        /// <returns> The List of Inventory </returns>
         List<Inventory> GetInventoryByStoreFront(int p_id);
+        /// <summary>
+        /// Gets Order History by Store
+        /// </summary>
+        /// <param name="p_storeId"> The Store Id</param>
+        /// <returns> The List of Orders </returns>
         List<Order> GetOrderHistoryByStoreId(int p_storeId);
+        /// <summary>
+        /// Gets All Customers
+        /// </summary>
+        /// <returns> The List of Customers </returns>
         List<Customer> GetAllCustomer();
+        /// <summary>
+        /// Task List based on await
+        /// </summary>
+        /// <returns></returns>
         Task <List<Customer>> GetAllCustomerAsync();
+        /// <summary>
+        /// Adds a customer to the databse
+        /// </summary>
+        /// <param name="p_customer"> The customer to add</param>
+        /// <returns> The added customer </returns>
         Customer AddCustomer(Customer p_customer);
         List<Product> GetAllProducts();
-        // List<LineItem> GetLineItemByStoreId(int p_id);
         LineItem GetLineItem(int p_id);
         Inventory ReplenishQuantity(int p_quantity, int p_storeFrontId, int p_productId);
         Customer UpdateCustomer(Customer p_customer);
